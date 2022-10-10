418a8fa36ae35524a11ccc4cd78b981a3107af37
marcusonline97 <67122089+marcusonline97@users.noreply.github.com>
2022-10-10 19:20:20 +0200

    Upload Version for triforce triangle

--- OPENGL_Project/Tester.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <stdbool.h>
-#include <stdio.h>
-
-void omg(int base, int height = 10) {
-	std::cout << "omg" << base * 2 + height * 2 << std::endl;
-	int;
-}
--- OPENGL_Project/WindowTriangleCreation2.cpp
+++ OPENGL_Project/WindowTriangleCreation2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
+//This code will use an IndexBuffer to produce a triangle with 1 inner triangle that isn't drawn.
 //Vertex Shader Source Code
 const char* VertexShaderSource = "#version 330 core \n"
 "layout (location = 0) in vec3 aPos; \n"
@@ -34,6 +34,16 @@ int main()
 		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
 		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
 		0.0f, +0.5f * float(sqrt(2)) / 3, 0.0f
+		-0.5f / 2, 0.5f *float(sqrt(3)), 6, 0.0f, //Inner Left
+		0.5f / 2, 0.5f *float(sqrt(3)), 6 , 0.0f,//Inner Right
+		0.0f, -0.5f * float(sqrt(3)), 3, 0.0f// Inner Down
+
+	};
+	GLuint indices[] =
+	{
+		0, 3 ,5, // Lower Left Triangle
+		3, 2 ,4, //Lower Right Triangle
+		5,4 , 1 //Upper Triangle
 	};
 	GLFWwindow* window = glfwCreateWindow(800, 800, "My TestBox", NULL, NULL); //2 first values for the size of the window
 	//ThirdValue is the text space for the name of the window. 
@@ -74,21 +84,26 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 	//Create a reference container to the Vertex Array Object and Vertex Buffer Object
-	GLuint VAO, VBO;
+	GLuint VAO, VBO, EBO;
 	//Generate the VAO and VBO with only 1 object each.
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	//Make the VAO the current Vertex Array Object by binding it.
 	glBindVertexArray(VAO);
 	//Bind the VBO specifying it's a Buffer Array.
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
-
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	glClearColor(0.87f, 0.63f, 0.37f, 1.0f); //The 3 first values signify RGB numbers and the last digit stand for the Alpha
 	glClear(GL_COLOR_BUFFER_BIT); //We tell the program that we want to clear the original colour.
@@ -96,22 +111,25 @@ int main()
 	glfwSwapBuffers(window); //Here we replace the windows base colour with the one we added above.
 
 
-	while (!glfwWindowShouldClose(window)) 
+	while (!glfwWindowShouldClose(window))
 
 		glClearColor(0.05f, 0.73f, 0.57f, 1.0f);
 
-		glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
+	glUseProgram(shaderProgram);
 
-		glBindVertexArray(VAO);
-		//Declare how many Arrays we want GL to draw.
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+	glBindVertexArray(VAO);
+	//Declare how many Arrays we want GL to draw.
+	glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 
-		glfwSwapBuffers(window);
-		//Take care of the glfwEvents
-		glfwPollEvents();
-	
+	glfwSwapBuffers(window);
+	//Take care of the glfwEvents
+	glfwPollEvents();
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	glDeleteProgram(shaderProgram);
 	glfwDestroyWindow(window);
 	glfwTerminate();
 
