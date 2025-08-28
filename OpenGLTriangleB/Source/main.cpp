9f6075798923c85b907245081960e2867b4cc848
marcusonline97 <Mjpersson97@gmail.com>
2025-08-28 20:09:56 +0200

    Update

--- OpenGLTriangleB/Source/main.cpp
+++ OpenGLTriangleB/Source/main.cpp
@@ -1,55 +1,161 @@
+//#include <GL/GL.h> //Legacy old Graphics library
+#include <GLFW/glfw3.h> //Extensions of OpenGL and more modern Window functionality wrapper
+//#include <GL/glu.h> Utility 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 #include <cmath>
-#include <GLFW/glfw3.h>
 #include <vector>
 
+static const float screenHeight = 900.0f;
+static const float screenWidth = 1000.0f;
 
-float screenHeight = 600.0f;
-float screenWidth = 800.0f;
 
+
+const char* vertSrc =
+"#version 330 core"
+"layout(location = 0) in vec3 aPos;\n"
+"uniform mat4 uMVP;\n"
+"void main() { "
+"    gl_Position = uMVP * vec4(aPos, 1.0);"
+"}";
+
+const char* fragSrc =
+"#version 330 core"
+"out vec4 FragColor"
+"uniform vec3 uColor;"
+"void main() {"
+"   FragColor = vec4(uColor, 1.0);"
+"}";
+
+#if banana 0
+GLuint Compileshader(GLenum type, const char* src)
+{
+	GLuint s = glCreateShader(type);
+	glShaderSource(s, 1, &src, nullptr);
+	glCompileShader(s);
+	GLint ok;
+	glGetShaderiv(s, GL_COMPILE_STATUS, &ok);
+	if (!ok)
+	{
+		char buf[512]; glGetShaderInfoLog(s, 512, nullptr, buf);
+		std::cerr << "Shader Compile Error: \n" << buf << "\n";
+	}
+	return s;
+}
+#endif
+//Variables
+float deltaTime = 0.0f;	// Time between current frame and last frame
+float lastFrame = 0.0;
+
+//Functions
 GLFWwindow* StartGLFW();
-void DrawCircle(float centerX, float centerY, float radius, int res);
+//Function Shapes
 
+//Function Prototypes
+void DrawCircle(float centerX, float centerY, float radius, int res);
+void Triangle();
+void MatrixUpdate();
+void DrawCirclein3D(float centerX, float centerY, float centerZ , float radius, int segments = 64);
+
+auto CameraLookMatrix = glm::lookAt(glm::vec3(0.0, 0.0, 5.0),
+	glm::vec3(0.0, 0.0, 0.0),
+	glm::vec3(0.0, 1.0, 0.0)
+
+);
+
+glm::mat4 projection = glm::perspective(glm::radians(45.0f), screenWidth / screenHeight, 0.1f, 100.0f); // Projection Matrix
+/*
+class Earth : Planet
+{
+
+};
+
+class Planet
+{
+
+};
+*/
+
+class Object
+{
+public:
+	
+	std::vector<float> position;
+	std::vector<float> velocity;
+	float radius;
+
+	Object(std::vector<float> position, std::vector<float> velocity, float radius = 15.0f)
+	{
+		this->position = position;
+		this->velocity = velocity;
+		this->radius = radius;
+	}
+
+	void accelerate(float x, float y)
+	{
+		velocity[0] += x;
+		velocity[1] += y;
+	}
+
+	void updatePos()
+	{
+		position[0] += velocity[0];
+		position[1] += velocity[1];
+	}
+	std::vector<float> DrawPlanet(int res = 10)
+	{
+		std::vector<float> vertices;
+
+	}
+	void DrawCircle(float centerX, float centerY, float radius, int res)
+	{
+		glBegin(GL_TRIANGLE_FAN);
+		glVertex2d(centerX, centerY);
+		for (int i = 0; i <= res; i++)
+		{
+			float angle = 2.0f * 3.14159f * (static_cast<float>(i) / res);
+			float x = centerX + cos(angle) * radius;
+			float y = centerY + sin(angle) * radius;
+			glVertex2d(x, y);
+		}
+		glEnd();
+	}
+};
 
 int main()
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
 	GLFWwindow* window = StartGLFW();
 	if (!window) return -1;
 
-	float centerX = screenWidth / 2;
-	float centerY = screenHeight / 2;
-	float radius = 50.0f;
-	int res = 100;
+	float currentFrame = glfwGetTime();
+	deltaTime = currentFrame - lastFrame;
+	lastFrame = currentFrame;
 
 	std::vector<float> position = { 400.0f, 300.0f };
-	std::vector<float> velocity = { 0.0f, 0.0f };
+	//std::vector<float> velocity = { 0.0f, 0.0f };
 	double lastTime = glfwGetTime();
 
+	std::vector<float> velocity = { 0.0f, 50.0f };
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
 	while (!glfwWindowShouldClose(window))
 	{
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		double now = glfwGetTime();
-		float deltaTime = static_cast<float>(now - lastTime);
-		lastTime = now;
-
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		DrawCircle(position[0], position[1], 50.0f, 50);
-
-		position[0] += velocity[0];
-		position[1] += velocity[1];
-		velocity[1] -= 9.81f * deltaTime * 10; // Gravity effect
-
-		if (position[1] < 0 || position[1] > screenHeight)
-		{
-			velocity[1] *= -0.95;
-		}
-		if(position[0] < 0 || position[0] > screenWidth)
-		{
-			velocity[0] *= -0.95;
-		}
+		//MatrixUpdate();
+		Triangle();
+		//DrawCirclein3D(0.0f, 0.0f, 0.0f, 1.5f, 128);
 
 
+
+		glFlush();
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 
@@ -75,22 +181,26 @@ GLFWwindow* StartGLFW()
 	}
 
 	glfwMakeContextCurrent(window); //Create a window
+	
 
 	glViewport(0, 0, static_cast<int>(screenWidth), static_cast<int>(screenHeight)); //Determining the size of the Window.
 
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-
-	glOrtho(0, screenWidth, 0, screenHeight, -1, 1);
-
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); //Black Background
 	return window;
 }
 
+void DrawCirclein3D(float centerX, float centerY, float centerZ, float radius, int segments)
+{
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < segments; i++)
+	{
+		float theta = 2.0f * 3.14159265359f * float(i) / float(segments);
+		float x = radius * cosf(theta);
+		float z = radius * sinf(theta);
 
+		glVertex3f(centerX + x, centerY, centerZ + z);
+	}
+	glEnd();
+}
 
 void DrawCircle (float centerX, float centerY, float radius, int res)
 {
@@ -104,4 +214,44 @@ void DrawCircle (float centerX, float centerY, float radius, int res)
 		glVertex2d(x, y);
 	}
 	glEnd();
+}
+void Triangle()
+{
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.0f, 0.0f, 0.0f); glVertex3f(-1.0f, -1.0f, 0.0f);
+	glColor3f(0.0f, 1.0f, 0.0f); glVertex3f(1.0f, -1.0f, 0.0f);
+	glColor3f(0.0f, 0.0f, 1.0f); glVertex3f(0.0f, 1.0f, 0.0f);
+	glEnd();
+}
+void DrawCircle3D(float centerX, float centerY, float centerZ, float radius, int segments)
+{
+	glColor3f(1.0f, 1.0f, 1.0f); // White color
+	glBegin(GL_LINE_LOOP);      // Use LINE_LOOP to connect all points
+
+	for (int i = 0; i < segments; ++i)
+	{
+		float theta = 2.0f * 3.14159265359f * float(i) / float(segments);
+		float x = radius * cosf(theta);
+		float z = radius * sinf(theta);
+		glVertex3f(centerX + x, centerY, centerZ + z); // Circle in XZ plane
+	}
+
+	glEnd();
+}
+
+
+void MatrixUpdate()
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glLoadMatrixf(glm::value_ptr(projection));
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glLoadMatrixf(glm::value_ptr(CameraLookMatrix));
+
+	//glTranslatef(0.0f, 0.0f, -5.0f);
+	//glRotatef(30.f, 1.0f, 0.0f, 0.0f);
+	//glPushMatrix();
+	//glPopMatrix();
 }
